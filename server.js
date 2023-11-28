8179d473952f39dac5aaa84c97b9e9af4f65890c
ArtspaceAI <151873916+ArtspaceAI@users.noreply.github.com>
2023-11-28 06:06:27 +0000

    Pending changes exported from your codespace

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', async (req, res) => {
+    try {
+        const url = req.query.url;
+        const inputPrompt = req.query.inputPrompt;
+
+        if (!url || !inputPrompt) {
+            return res.status(400).send('URL and inputPrompt parameters are required.');
+        }
+
+        const output = await runScript(url, inputPrompt);
+
+        // Send the script output as part of the HTML response
+        res.send(`<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Script Output</title>
+        </head>
+        <body>
+            <h1>Script Output</h1>
+            <pre id="output">${output}</pre>
+        </body>
+        </html>`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+async function runScript(url, inputPrompt) {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    // Your existing script code here
+    await page.goto(url);
+    await page.waitForSelector("#prompt-form > label > textarea", { visible: true, timeout: 20000 });
+    await page.type("#prompt-form > label > textarea", inputPrompt);
+    await page.click('#submit');
+    await page.waitForTimeout(35000);
+
+    const elementsToCopy = [];
+    for (let i = 87; i <= 94; i++) {
+        const elementSelector = `#component-${i} > div.wrapper.svelte-nab2ao > div > div > div.message-row.bubble.bot-row.svelte-1pjfiar > div.message.bot.svelte-1pjfiar.message-bubble-border > button > span`;
+
+        const elementText = await page.evaluate((selector) => {
+            const element = document.querySelector(selector);
+            return element ? element.textContent.trim() : null;
+        }, elementSelector);
+        elementsToCopy.push(elementText);
+    }
+
+    // Close the browser
+    await browser.close();
+
+    // Return the script output
+    return `Copied Text: ${elementsToCopy.join(', \n***************************************\n***************************************\n')}`;
+}
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
